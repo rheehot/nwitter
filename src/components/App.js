e1af29a78180a7b3c767f7b3365ffa93c71dd085
peter L <peter.l@uvaper.com>
2020-10-04 19:20:27 +0900

    #2.2 recap

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
-import React, {useState} from 'react';
-import AppRouter from 'components/Router';
-import firebase from "fbase"
-import {authService} from "fbase";
+import React, { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
+  // console.log(authService.currentUser);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
+}
 
-  console.log(authService.currentUser);
-
-  const [isLoggedIn , setLoggedIn] = useState(false);
-  return 
-  <>
-    <AppRouter isLoggedIn={isLoggedIn}/>
-   <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
-  </>; 
-
-} 
 export default App;
